fe40cbee32084c578575e354a1933e2ad6a81da4
Eduardo Lowensohn <gunchosoft@gmail.com>
2019-07-06 17:06:07 -0300

    mystack1

---
+++ StacksandQueues/mystack/mystack.rb
@@ -0,0 +1,33 @@
+class MyStack
+    attr_accessor :top
+  
+    def initialize
+      @stack = Array.new
+      self.top = nil
+    end
+  
+    def push(item)
+        @stack << item
+        self.top = @stack.last
+    end
+  
+    def pop # Last In First Out
+        
+      item = self.top
+        if !empty?
+      
+       @stack.delete_at(@stack.length - 1)
+       self.top = @stack.last
+      end
+     item 
+    end
+
+    def empty?
+      if @stack == [] || @stack.length === 0
+        true
+      else
+        false
+      end
+
+    end
+  end
